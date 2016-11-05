b183a6e36836576baa6f431c23abf590782d5087
kurbhatt <info.keyurbhatt@gmail.com>
2016-11-05 14:13:28 +0530

    Changed text for route param.
    Commit: 004

--- src/app/route-param-1/route-param-1.component.ts
+++ src/app/route-param-1/route-param-1.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-route-param-1',
-  template: `<h2>Route Param</h2>`
+  template: `<h2>Route Param Home</h2>`
 })
 export class RouteParam1Component implements OnInit {
 
